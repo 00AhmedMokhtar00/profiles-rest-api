387abd669d96797790287804d819bdd177dbee4e
Ahmed Mokhtar <ahmedmokhtar@Ahmeds-MacBook-Pro.local>
2023-04-16 00:48:55 +0200

    enabled admin use

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -13,6 +13,7 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name=name)
         user.set_password(password)
         user.save(using=self._db)
+        return user
         
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
